1df5f9d2a432781cd7a71a94e388cfcc59219541
luis <lfilipe780@gmail.com>
2021-07-09 16:27:11 -0300

    Att media móvel

--- appCovid.py
+++ appCovid.py
@@ -4,75 +4,65 @@ import matplotlib.pyplot as plt
 import utils
 
 # Load Data
-df = utils.read_csv()
+df = utils.readCSV()
 
 st.title('Números da COVID-19 no Brasil')
 
-date_end = df['data'].iloc[-1].date()
-date_start = df['data'].iloc[0].date()
+dateEnd = df['data'].iloc[-1].date()
+dateStart = df['data'].iloc[0].date()
 
-date = st.date_input('Selecione a Data', date_end, date_start,
-                     date_end, help='Coloque a data que queira gerar o relatório.')
+date = st.date_input('Selecione a Data', dateEnd, dateStart, dateEnd)
 
 # Codigos UF
-list_uf = [11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25,
-           26, 27, 28, 29, 31, 32, 33, 35, 41, 42, 43, 50, 51, 53, 76]
+listUF = [11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25,26, 27, 28, 29, 31, 32, 33, 35, 41, 42, 43, 50, 51, 53, 76]
 
-df_processed = pd.DataFrame(columns=['Data', 'Estado', 'CasosAcumulado', 'MediaMovelCasosAtual', 'MediaMovelCasosAnterior',
-                                     'Situacao', 'Percentual', 'ObitosAcumulado', 'MediaMovelObitosAtual', 'MediaMovelObitosAnterior', 'Situacao', 'Percentual'])
+dfProcessed = pd.DataFrame(columns=['Data', 'Estado', 'CasosAcumulado', 'MediaMovelCasosAtual', 'MediaMovelCasosAnterior','Situacao', 'Percentual(%)', 'ObitosAcumulado', 'MediaMovelObitosAtual', 'MediaMovelObitosAnterior', 'Situacao', 'Percentual(%)'])
 
-for uf in list_uf:
+for uf in listUF:
     df_mask = df['coduf'] == uf
-    filtered_df = df[df_mask]
-    filtered_df = filtered_df.iloc[:, lambda df: [
-        1, 3, 7, 8, 9, 10, 11, 12, 13, 14, 15]]
-    filtered_df = filtered_df.sort_values(by=['data'])
-    list_data = utils.itens_calculate(
-        filtered_df, date, date_start, uf)
-    df_processed.loc[len(df_processed)] = list_data
+    filteredDf = df[df_mask]
+    filteredDf = filteredDf.iloc[:, lambda df: [1, 3, 7, 8, 9, 10, 11, 12, 13, 14, 15]]
+    filteredDf = filteredDf.sort_values(by=['data'])
+    list_data = utils.itensCalculate(filteredDf, date, dateStart, uf)
+    dfProcessed.loc[len(dfProcessed)] = list_data
 
-# Divisao dos Dataframes 
-df_processed_casos = df_processed.iloc[:, lambda df: [0, 1, 2, 3, 4, 5, 6]]
-df_processed_obitos = df_processed.iloc[:, lambda df: [0, 1, 7, 8, 9, 10, 11]]
+# Divisao dos Dataframes
+dfProcessedCasos = dfProcessed.iloc[:, lambda df: [0, 1, 2, 3, 4, 5, 6]]
+dfProcessedObitos = dfProcessed.iloc[:, lambda df: [0, 1, 7, 8, 9, 10, 11]]
 
 
 #Casos
-st.header('Número de Casos de COVID-19 no Brasil')
-st.dataframe(df_processed_casos)
+st.write("Data(Dia/Mês/Ano):",str(date.day),"/",str(date.month),"/",str(date.year))
+st.header('Número de Casos de COVID-19 no Brasil '+str(date.day)+"/"+str(date.month)+"/"+str(date.year))
+st.write("Obs: Número total do Brasil ao final da tabela")
+st.dataframe(dfProcessedCasos)
 
-st.header('Casos de COVID-19 x Estado')
-df_ordenado = df_processed_casos.sort_values(
-    by=['CasosAcumulado'], ascending=True)
+st.header('Casos Acumulados de COVID-19 por Estado')
+dfOrdenado = dfProcessedCasos.sort_values(by=['CasosAcumulado'], ascending=True)
 fig, ax = plt.subplots()
-ax.barh(df_ordenado['Estado'][:-1],
-        df_ordenado['CasosAcumulado'][:-1])
+ax.barh(dfOrdenado['Estado'][:-1],dfOrdenado['CasosAcumulado'][:-1])
 st.pyplot(fig)
 
-st.header('Média Móvel de Casos x Estado')
-df_ordenado = df_processed_casos.sort_values(
-    by=['MediaMovelCasosAtual'], ascending=True)
+st.header('Média Móvel de Casos por Estado')
+dfOrdenado = dfProcessedCasos.sort_values(by=['MediaMovelCasosAtual'], ascending=True)
 fig, ax = plt.subplots()
-ax.barh(df_ordenado['Estado'][:-1],
-        df_ordenado['MediaMovelCasosAtual'][:-1])
+ax.barh(dfOrdenado['Estado'][:-1],dfOrdenado['MediaMovelCasosAtual'][:-1])
 st.pyplot(fig)
 
 
 #Mortes
-st.header('Número de Mortes de COVID-19 no Brasil')
-st.dataframe(df_processed_obitos)
+st.header('Número de Óbitos por COVID-19 no Brasil '+str(date.day)+"/"+str(date.month)+"/"+str(date.year))
+st.write("Obs: Número total do Brasil ao final da tabela")
+st.dataframe(dfProcessedObitos)
 
-st.header('Óbitos por COVID-19 x Estado')
-df_ordenado = df_processed_obitos.sort_values(
-    by=['ObitosAcumulado'], ascending=True)
+st.header('Óbitos Acumulados por COVID-19 por Estado')
+dfOrdenado = dfProcessedObitos.sort_values(by=['ObitosAcumulado'], ascending=True)
 fig, ax = plt.subplots()
-ax.barh(df_ordenado['Estado'][:-1],
-        df_ordenado['ObitosAcumulado'][:-1])
+ax.barh(dfOrdenado['Estado'][:-1],dfOrdenado['ObitosAcumulado'][:-1])
 st.pyplot(fig)
 
-st.header('Média Móvel de Óbitos x Estado')
-df_ordenado = df_processed_obitos.sort_values(
-    by=['MediaMovelObitosAtual'], ascending=True)
+st.header('Média Móvel de Óbitos por Estado')
+dfOrdenado = dfProcessedObitos.sort_values(by=['MediaMovelObitosAtual'], ascending=True)
 fig, ax = plt.subplots()
-ax.barh(df_ordenado['Estado'][:-1],
-        df_ordenado['MediaMovelObitosAtual'][:-1])
+ax.barh(dfOrdenado['Estado'][:-1],dfOrdenado['MediaMovelObitosAtual'][:-1])
 st.pyplot(fig)
--- utils.py
+++ utils.py
@@ -2,116 +2,106 @@ import pandas as pd
 import glob
 from datetime import datetime, timedelta
 
-# read all csv in folder 'dataset'
+# Leitura dos arquivos da pasta dataset
 
-
-def read_csv():
-    list_csv = []
-    name_path = 'dataset'
+def readCSV():
+    listCSV = []
+    namePath = 'dataset'
     # Select all csv in folder selected
-    names_files = glob.glob(name_path + "/*.csv")
+    namesFiles = glob.glob(namePath + "/*.csv")
     # join all them
-    for filename in names_files:
+    for filename in namesFiles:
         df = pd.read_csv(filename, sep=';')
-        df_mask = df['codmun'].isnull()
-        filtered_df = df[df_mask]
-        list_csv.append(filtered_df)
+        dfMask = df['codmun'].isnull()
+        filtered_df = df[dfMask]
+        listCSV.append(filtered_df)
 
-    frame = pd.concat(list_csv, axis=0, ignore_index=True)
+    frame = pd.concat(listCSV, axis=0, ignore_index=True)
     frame['data'] = pd.to_datetime(frame['data'])  # .dt.strftime('%d/%m/%Y')
     return frame
 
-# Calculate all itens
-
-
-def itens_calculate(df, date, date_start, uf):
-    list_all = []
+def itensCalculate(df, date, dateStart, uf):
+    all = []
     mask = df['data'] == date.strftime('%Y-%m-%d')
-    df_aux = df[mask]
+    dfAux = df[mask]
     # Date
-    list_all.append(date)
+    all.append(date)
 
     # State
     if uf == 76:
-        list_all.append('Brasil')
+        all.append('Brasil')
     else:
-        list_all.append(df['estado'].iloc[0])
+        all.append(df['estado'].iloc[0])
 
     # CasosAcumulado
-    list_all.append(int(df_aux['casosAcumulado'].iloc[0]))
+    all.append(int(dfAux['casosAcumulado'].iloc[0]))
     # MediaMovelCasosAtual, MediaMovelCasosAnterior, Situação, Porcentagem
-    for i in moving_average(df, date, date_start, 0):
-        list_all.append(i)
+    for i in movingAverage(df, date, dateStart, 0):
+        all.append(i)
 
     # ObitosAcumulados
-    list_all.append(df_aux['obitosAcumulado'].iloc[0])
+    all.append(dfAux['obitosAcumulado'].iloc[0])
     # MediaMovelObtitosAtual, MediaMovelObitosAnterior, Situação, Porcentagem
-    for j in moving_average(df, date, date_start, 1):
-        list_all.append(j)
+    for j in movingAverage(df, date, dateStart, 1):
+        all.append(j)
 
-    return list_all
+    return all
 
 
 # number = 0 -> Casos or number != 0 -> Óbitos
 
 
-def moving_average(df, date, date_start, number):
-    list_all = []
+def movingAverage(df, date, dateStart, number):
+    all = []
     if number == 0:
-        df_aux = df[['data', 'casosAcumulado']]
+        dfAux = df[['data', 'casosAcumulado']]
     else:
-        df_aux = df[['data', 'obitosAcumulado']]
+        dfAux = df[['data', 'obitosAcumulado']]
 
     # MediaMovelAtual
-    mean_today = average_call(df, date, date_start, number)
+    mean_today = averageCall(df, date, dateStart, number)
     # MediaMovelAnterior
-    mean_before = average_call(
-        df, date - timedelta(days=1), date_start, number)
+    mean_before = averageCall(df, date - timedelta(days=1), dateStart, number)
 
-    list_all.append(int(mean_today))
-    list_all.append(int(mean_before))
+    all.append(int(mean_today))
+    all.append(int(mean_before))
 
     # Situação e Porcentagem of each moving-average
     if mean_before == 0:
         if mean_today != 0:
-            list_all.append('Aumento')
-            list_all.append(100)
+            all.append('Aumento')
+            all.append(100)
         else:
-            list_all.append('Estabilidade')
-            list_all.append('-')
+            all.append('Estabilidade')
+            all.append('-')
     elif mean_today/mean_before > 1:
-        list_all.append('Aumento')
-        list_all.append(round(((mean_today/mean_before - 1)*100), 4))
+        all.append('Aumento')
+        all.append(round(((mean_today/mean_before - 1)*100), 4))
     elif mean_today/mean_before < 1:
-        list_all.append('Diminuicao')
-        list_all.append(round(abs(mean_today/mean_before - 1)*100, 4))
+        all.append('Diminuicao')
+        all.append(round(abs(mean_today/mean_before - 1)*100, 4))
     else:
-        list_all.append('Estabilidade')
-        list_all.append(round((mean_today/mean_before - 1)*100, 4))
+        all.append('Estabilidade')
+        all.append(round((mean_today/mean_before - 1)*100, 4))
 
-    return list_all
+    return all
 
-
-# Calculate Average Moving
-
-
-def average_call(df, date, date_start, number):
+def averageCall(df, date, dateStart, number):
     colum = ''
     if number == 0:
-        colum = 'casosAcumulado'
+        colum = 'casosNovos'
     else:
-        colum = 'obitosAcumulado'
+        colum = 'obitosNovos'
 
     # First 7 days
-    if date.strftime('%Y-%m-%d') < (date_start + timedelta(days=7)).strftime('%Y-%m-%d'):
+    if date.strftime('%Y-%m-%d') < (dateStart + timedelta(days=7)).strftime('%Y-%m-%d'):
         mask = (df['data'] <= date.strftime('%Y-%m-%d'))
-        df_aux = df[mask]
-        return df_aux[colum].sum()/7
+        dfAux = df[mask]
+        return dfAux[colum].sum()/7
 
     # After
     else:
         # Select part of dataframe that need to calculate mean
-        mask = (df['data'] <= date.strftime(
-            '%Y-%m-%d')) & (df['data'] > (date - timedelta(days=7)).strftime('%Y-%m-%d'))
-        df_aux = df[mask]
-        return df_aux[colum].mean()
+        mask = (df['data'] <= date.strftime('%Y-%m-%d')) & (df['data'] > (date - timedelta(days=7)).strftime('%Y-%m-%d'))
+        dfAux = df[mask]
+        return dfAux[colum].mean()
